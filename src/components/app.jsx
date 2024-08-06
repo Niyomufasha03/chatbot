8dc3437f2237109491da1210ecc45d3638f7a16e
Niyomufasha03 <DNiyomufasha@ppg.com>
2024-08-06 16:26:44 -0400

    Update app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./authentication/login";
+import Login from "./authentication/Login";
 import ChatRoom from "./chat/chatroom";
 import RequireAuth from "./authentication/auth_component";
 
